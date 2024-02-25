8976adc1b1e042723386a6e9ed2388d4a6059e67
Попов Роман <rv_popov@controlsystems.ru>
2024-02-25 10:42:51 +0700

    1

--- Lifes-log/Lifes-log/LLEvents/UNote.xaml.cs
+++ Lifes-log/Lifes-log/LLEvents/UNote.xaml.cs
@@ -8,39 +8,39 @@ namespace Lifes_log.LLEvents
     public partial class UNote 
     {
         private readonly string lang = (App.Current as App).lang;
-        private readonly short ntp;
         private int cd;
         protected string cname;
         protected EventBody Bd;
         private readonly Event et;
-        private readonly DateTime dt;
+        private DateTime dt;
 
-        public UNote(int cd, Event et)
+        public UNote(int cds, Event ets)
         {
             InitializeComponent();
-            this.cd = cd; this.et = et;
-            var cmd = (App.Current as App).NpDs.CreateCommand(
-                $@"Select l.comment,lt.{lang}_name as nm,lt.class_name,l.event_type_id
-                From ll_event l join ll_event_type lt on l.event_type_id = lt.id
-                Where l.id = {this.cd}");
+            cd = cds;
+            et = ets;
+            var cmd = ((App)App.Current).NpDs.CreateCommand(
+                $"Select l.comment,lt.{lang}_name as nm,lt.class_name,l.event_type_id "+
+                "From ll_event l join ll_event_type lt on l.event_type_id = lt.id "+
+                "Where l.id = {cd}");
             var rd = cmd.ExecuteReader();
             rd.Read();
             GNote.Text = rd["comment"].ToString();
             TypeNote.Text = rd["nm"].ToString();
             cname = rd["class_name"].ToString();
-            ntp = (short)rd["event_type_id"];
+            var ntp = (short)rd["event_type_id"];
             rd.Close();
             UBody(cmd, this.cd, ntp);
         }
 
-        public UNote(short tp, Event et)
+        public UNote(short tp, Event ets)
         {
-            ntp = tp; this.et = et;
+            var ntp = tp; et = ets;
             InitializeComponent();
-            cd = 0; dt = this.et.Dt;
-            DelBt.Visibility = Microsoft.UI.Xaml.Visibility.Collapsed;
+            cd = 0; dt = et.Dt;
+            DelBt.Visibility = Visibility.Collapsed;
             var cmd = (App.Current as App).NpDs.CreateCommand(
-            $@"Select {lang}_name as nm,class_name From ll_event_type Where id = {ntp}");
+            $"Select {lang}_name as nm,class_name From ll_event_type Where id = {ntp}");
             var rd = cmd.ExecuteReader();
             rd.Read();
             TypeNote.Text = rd["nm"].ToString();
@@ -49,7 +49,7 @@ namespace Lifes_log.LLEvents
             UBody(cmd, cd, ntp);
         }
 
-        private void UBody(NpgsqlCommand cmd, Int32 cd, Int16 ntp)
+        private void UBody(NpgsqlCommand cmd, int cd, short ntp)
         {
             /*    switch (cname)
             {
@@ -73,7 +73,7 @@ namespace Lifes_log.LLEvents
             if (Bd == null) SetGNoteFocus(); else Bd.GetFocus();
         }
 
-        private void Log_Click(object _1, Microsoft.UI.Xaml.RoutedEventArgs _2)
+        private void Log_Click(object  _1, RoutedEventArgs _2)
         {
             var cmt = Bd == null ? "" : Bd.ToString();
             //var tr = sq.BeginTransaction(IsolationLevel.ReadCommitted);
@@ -87,16 +87,16 @@ namespace Lifes_log.LLEvents
                 et.Code = cd;
                 rd.Close();
                 cmd.CommandText =
-                    $@"@INSERT INTO ll_event (  id,server_time,    event_time,comment, description, event_type_id)
-                                    values   ({cd}, localtime,'{dt:yyyyMMdd}','{cmt}','{GNote.Text}',      {ntp})";
+                    $"INSERT INTO ll_event (  id,server_time,    event_time,comment, description, event_type_id)"+
+                                 "values   ({cd}, localtime,'{dt:yyyyMMdd}','{cmt}','{GNote.Text}',      {ntp})";
                 cmd.ExecuteNonQuery();
             }
             else
             {
                 var cmd = (App.Current as App).NpDs.CreateCommand(
-                $"Update LLEvent Set Comment='{cmt}',Descr='{GNote.Text}' Where Code={cd}");
+                $"Update ll_event Set comment='{cmt}',description='{GNote.Text}' Where id={cd}");
                 cmd.ExecuteNonQuery();
-                cmd.CommandText = $"Delete From LLEventValue Where EventCode={cd}";
+                cmd.CommandText = $"Delete From ll_value Where event_id={cd}";
                 cmd.ExecuteNonQuery();
             }
             if (Bd != null)
@@ -108,7 +108,7 @@ namespace Lifes_log.LLEvents
             et.Collapse();
         }
 
-        private void Delete_Click(object _1, Microsoft.UI.Xaml.RoutedEventArgs _2)
+        private void Delete_Click(object _1, RoutedEventArgs _2)
         {
             /*using (var sq = new SqlConnection((App.Current as App).ConStr))
             {
@@ -124,7 +124,7 @@ namespace Lifes_log.LLEvents
             et.Collapse();
         }
 
-        private void Cancel_Click(object _1, Microsoft.UI.Xaml.RoutedEventArgs _2)
+        private void Cancel_Click(object _1, RoutedEventArgs _2)
         {
             et.Collapse();
         }
